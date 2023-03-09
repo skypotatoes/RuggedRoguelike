58ff2284a8e86273d728ce7cf7f38288b7d20235
skypotatoes <61054679+skypotatoes@users.noreply.github.com>
2023-03-09 02:23:40 +0000

    fixes aiming

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ let squadSize = 5;
 
 const gameObject = {};
 //gameObject.squadMates = {};
-gameObject.world =[];
+gameObject.world = [];
 gameObject.waypointX = width / 2;
 gameObject.waypointY = height / 2;
 gameObject.selected = []
@@ -34,38 +34,39 @@ gameObject.prevWaypointY = 0;
 const Soldier = class {
   constructor(x, y, name, faction) {
     this.x = x,
-    this.y = y;
+      this.y = y;
     this.name = name;
     this.faction = faction;
     this.angle = 0;
-    this.theta =0;
+    this.theta = 0;
 
   }
+  gunAngle = 0;
+  angleToEnemy = 0;
   radius = radius;
   perceptionBubble = perceptionBubble;
   perceptionArray = [];
   waypointX = this.x;
   waypointY = this.y;
   draw = function () {
-    //console.log(gameObject)
 
 
-  
     let perceptionBubble = this.perceptionBubble;
     let x = this.x
     let y = this.y
     let angle = this.angle
+    let angleToEnemy = this.angleToEnemy
     let radius = this.radius
     let adj = this.waypointX - this.x; //left/right dist to waypoint
     let opp = this.waypointY - this.y; // up/down distance to waypoint 
     let hyp = sqrt((opp * opp) + (adj * adj)); // only a distance can't give the direction of hyp, fudge coming later...
 
     this.hyp = hyp;
-    let theta; 
-    if (hyp===0){theta = 0}else{theta = Math.asin(opp / hyp)}
+    let theta;
+    if (hyp === 0) { theta = 0 } else {
+      theta = Math.asin(opp / hyp)
+    }
 
-    
-    
     this.theta = theta;
     //here comes the fudge:
     if (adj < 0) {
@@ -74,8 +75,8 @@ const Soldier = class {
     } //this corrects for if waypoint is to the left of the soldier; it flips the angle and for some reason I needed to shift it by Pi. Ah, who doesn't love trig?
 
     push(); //draws the green circle
-    if (this.faction==='enemy'){fill(enemies)}else{ fill(friendlies)}
-    circle(x, y, 2*radius);
+    if (this.faction === 'enemy') { fill(enemies) } else { fill(friendlies) }
+    circle(x, y, 2 * radius);
     noFill();
     pop(); //end of green circle
 
@@ -83,28 +84,59 @@ const Soldier = class {
     stroke(blue);
     line(x,
       y,
-      x + (radius * cos(angle)),
-      y + (radius * sin(angle))
+      x + (radius * cos(this.angle)),
+      y + (radius * sin(this.angle))
     );
     pop(); //end of orientation line
- 
-      if(gameObject.selected.includes(this)){
 
-        push(); // a perception bubble drawn around the soldier
-        noFill();
-        stroke(green);
-        circle(x, y, perceptionBubble * 2)
-        pop();
-    
-        push();
-        stroke(blue); // draws a blue line between bots when they are in the perceptionArray
-        for (let i = 0; i < this.perceptionArray.length; i++) {
-          let other = this.perceptionArray[i];
-          line(x, y, other.x, other.y)// 
-        }
-        pop();
+    push(); //draws the gun line
+    strokeWeight(2);
+    stroke("black");
+    line(x,
+      y,
+      x + (radius * cos(this.gunAngle)),
+      y + (radius * sin(this.gunAngle))
+    );
+    pop(); //end of gun line
 
-      }
+    if (gameObject.selected.includes(this)) {
+
+      push(); // a perception bubble drawn around the soldier
+      noFill();
+      stroke(green);
+      circle(x, y, perceptionBubble * 2)
+      pop();
+
+      // push();
+      // stroke(blue); // draws a blue line between bots when they are in the perceptionArray
+      // for (let i = 0; i < this.perceptionArray.length; i++) {
+      //   let other = this.perceptionArray[i];
+      //   line(x, y, other.x, other.y)// 
+      // }
+      // pop();
+
+      let angleToEnemy = this.angleToEnemy
+      // push();
+      // strokeWeight(4)
+      // stroke(red); // draws a red line to closest enemy
+      // if (this.closestEnemy) {
+      //   let other = this.closestEnemy
+      //   line(x, y, other.x, other.y)
+      //   //console.log('x: '+x)
+      //   //console.log(this.angleToEnemy)
+      //   let distance = dist(this.x, this.y, other.x, other.y)
+      //   stroke(green)
+      //   line(
+      //     x,
+      //     y,
+      //     x + distance * cos(angleToEnemy),
+      //     y + distance * sin(angleToEnemy))
+      
+
+      // }
+      // pop();
+
+    }
 
 
   }
@@ -115,8 +147,8 @@ const Soldier = class {
     let selectedArray = gameObject.selected
     if (distanceToClick <= radius && !selectedArray.includes(this)) {
       if (selectedArray.length > 0) {
-       // console.log('soldier '+this.name+' selected')
-        console.log(selectedArray[0].perceptionArray)
+        // console.log('soldier '+this.name+' selected')
+        // console.log(selectedArray[0].closestEnemy)
         gameObject.selected[0].waypointX = gameObject.prevWaypointX;
         gameObject.selected[0].waypointY = gameObject.prevWaypointY;
         gameObject.selected.pop();
@@ -135,65 +167,104 @@ const Soldier = class {
       this.waypointX = gameObject.waypointX;
       this.waypointY = gameObject.waypointY
     };
-    //     if (gameObject.selected.length>0){console.log(gameObject.selected[0].name)}
-
-    //console.log(selectedArray[0])
-
-
 
     //start of perception
     this.perceptionArray = [];
     // To be perceived by the soldier, an object must have a distance d < bubbleRadius
     //first get a list of all other things in the world array 
+    for (let i = 0; i < gameObject.world.length; i++) {
+      let other = gameObject.world[i];
+      let distance = dist(this.x, this.y, other.x, other.y)
+      if (distance < this.perceptionBubble && !this.perceptionArray.includes(other)) {
+        this.perceptionArray.push(other);
+      }
+    }
 
-for (let i = 0; i < gameObject.world.length; i++) {
-  let other = gameObject.world[i];
-  let distance = dist(this.x,this.y,other.x,other.y)
-
-
-
-if (distance < this.perceptionBubble && !this.perceptionArray.includes(other)) {
-         this.perceptionArray.push(other);}
-}
-
-    
-
-
+    this.closestEnemy = 0;
+    //this.angleToEnemy = 0;
     //personal space - loop thru the perceptionArray and anyone within the radius of the soldier causes a force to be exerted
     for (let i = 0; i < this.perceptionArray.length; i++) {
-
-      if (this.perceptionArray[i] !==this) {
+      if (this.perceptionArray[i] !== this) {
         let other = this.perceptionArray[i]
-        console.log(this.x)
         let myX = this.x;
         let myY = this.y;
         let xDist = myX - other.x;
         let yDist = myY - other.y;
-        let dist = Math.sqrt((xDist * xDist) + (yDist * yDist));
+        let distance = Math.sqrt((xDist * xDist) + (yDist * yDist));
         let theta;
-        if (dist === 0) { theta = 0 } else {
-          theta = Math.asin(yDist / dist)
+        if (distance === 0) { theta = 0 } else {
+          theta = Math.asin(yDist / distance)
         }
-        //console.log("dist: "+dist+" radius: "+radius)
+
         //collision detection:
-        if (dist <= 2*radius) {
+        if (distance <= 2 * radius) {
           this.x -= cos(theta);
           this.y += sin(theta);
         }
+
+
+        if (other.faction !== this.faction) {
+          if (!this.closestEnemy) {
+            this.closestEnemy = (other)
+          }
+          if (this.closestEnemy) {
+            const distanceToOther = dist(this.x, this.y, other.x, other.y);
+            const distToCurrentClosest = dist(this.x, this.y, this.closestEnemy.x, this.closestEnemy.y)
+            if (distanceToOther < distToCurrentClosest) {
+              this.closestEnemy = (other)
+            }
+          }
+        }
+
+        if (this.closestEnemy) {
+          let enemy = this.closestEnemy
+          let adj = enemy.x - this.x;
+          let opp = enemy.y - this.y;
+          let hyp = dist(this.x, this.y, enemy.x, enemy.y)
+          if (hyp === 0) { this.angleToEnemy = 0 } else { this.angleToEnemy = Math.asin(opp / hyp) }
+          
+          
+          if (adj < 0) {  // ah, fudge
+            this.angleToEnemy = -this.angleToEnemy - PI;
+            //smallestAngle = -smallestAngle;
+          } //this corrects for x axis 
+          
+          let ang = this.gunAngle - this.angleToEnemy
+          let angPlus2pi = ang + 2 * PI;
+          let angMinus2pi = ang - 2 * PI;
+          //Now we have 3 distances in radians ... we want to select the one with the lowest absolute value and use it...but we want to keep the + or - sign when we do! I think I need to build a function!!!
+          let smallestAngle = pickSmallestByMagnitude(ang, angPlus2pi, angMinus2pi);
+          // const diff = diffUsual
+
+        
+          //  if (diff < 0) {
+          //    this.gunAngle -= 1 * PI / 180;
+          //  };
+          // if (diff > 0) {
+          //   this.gunAngle += 1 * PI / 180;
+          // };
+
+          // if (adj < 0) {  // ah, fudge
+          //   this.angleToEnemy = -this.angleToEnemy - PI;
+          //   //smallestAngle = -smallestAngle;
+          // } //this corrects for x axis 
+
+          if (smallestAngle > 0){
+            this.gunAngle -= PI/180
+          }
+          if (smallestAngle < 0 ){
+            this.gunAngle += PI/180
+          }
+
+
+        }
       }
-
-
-
     }
 
 
-
-    //these two if statements will restart the angle from 0 if it goes +/- 360 degrees(i.e. 2*Pi radians)
-    // console.log(gameObject.squadMates[name].soldierAngle <= -2 * PI);
     let angle = this.angle
     let theta = this.theta
     let hyp = this.hyp
-
     //soldier direction behaviour - orient toward the waypoint // need to solve the problem of spinning the wrong way when angle is near to zero
     //first calculate the difference in radians between angle and theta:
     let diffUsual = angle - theta
@@ -202,106 +273,68 @@ if (distance < this.perceptionBubble && !this.perceptionArray.includes(other)) {
     let diffPlus2pi = diffUsual + 2 * PI;
     let diffMinus2pi = diffUsual - 2 * PI;
     //Now we have 3 distances in radians ... we want to select the one with the lowest absolute value and use it...but we want to keep the + or - sign when we do! I think I need to build a function!!!
-    const diff = pickSmallestByMagnitude(diffUsual, diffPlus2pi, diffMinus2pi)
-    //console.log("diff: "+diff)
-    //console.log(gameObject.squadMates[name].soldierAngle )
-    //console.log(diff)
+    const diff = pickSmallestByMagnitude(diffUsual, diffPlus2pi, diffMinus2pi);
     if (diff < 0) {
       this.angle += 8 * PI / 180;
     };
     if (diff > 0) {
       this.angle -= 8 * PI / 180;
     };
-    // console.log("soldier x,y: "+soldier.x+","+soldier.y)
-    // console.log("waypoint x,y: "+waypoint.x+","+waypoint.y)
-    //soldier movement behaviour; towards waypoint with x/y components scaled for orientation
-    //console.log(Math.cos(gameObject.squadMates[name].soldierAngle));
-
-    //console.log(hyp)
-    //console.log(gameObject.squadMates[name].x)
 
 
-    if (hyp > 50) {  // this if statements make them stop when reaching waypoint
+    if (hyp > 25) {  // this if statements make them stop when reaching waypoint
       this.x += cos(angle);
       this.y += sin(angle);
     }
 
-    //--->END OF SOLDIER  <---\\
 
-    if (angle >= 2 * PI) {
-
-      angle = 0;
+    //these two if statements will restart the angle from 0 if it goes +/- 360 degrees(i.e. 2*Pi radians)
+    if (this.angle >= 2 * PI) {
+      this.angle = 0;
     };
-    if (angle <= -2 * PI) {
-      angle = 0;
+    if (this.angle <= -2 * PI) {
+      this.angle = 0;
     };
-    if (theta >= 2 * PI) {
-      theta = 0;
+    if (this.theta >= 2 * PI) {
+      this.theta = 0;
     };
-    if (theta <= -2 * PI) {
-      theta = 0;
+    if (this.theta <= -2 * PI) {
+      this.theta = 0;
     };
 
-  }
 
-
-
-
-}
-
+  }//end of soldier behaviour function
+}//end of soldier class
+//--->END OF SOLDIER  <---\\
 
 function spawnSoldier(x, y, name, faction) {
-
   const recruit = new Soldier(x, y, name, faction);
-
   gameObject.world.push(recruit);
-
-
-  //console.log(gameObject)
 }
 
 
 
 function spawnSquad(num, x, y, faction) { //spawns a squad of num soldiers on the screen around the point x,y
   if (num === 0) { return; };
-
-  // if (num === 2){
-  //   spawnSoldier(x, y, 'Albert');
-  //   spawnSoldier(x+100, y-100, 'Charlie');
-  // }
-//spawn within a 500x500 space around the point
   len = 500;
   wid = 500;
-  let xSpace=len/num;
-  let ySpace=wid/num;
-
- 
+  let xSpace = len / num;
+  let ySpace = wid / num;
   for (let i = 0; i < num; i++) {
-
-    // spawnSoldier(i*width/num,y,i);
-    //console.log("#soldier "+i+" spawned")
-    //console.log(i)
-
-    let spawnX = x+i*xSpace
-    let spawnY =y+ySpace*Math.random()
+    let spawnX = i * xSpace + 50
+    let spawnY = y + ySpace * Math.random()
     spawnSoldier(spawnX, spawnY, i, faction)
-
-
-
   }
-}//1 soldier will just spawn on the coords
+}
 
 function setup() {
   cnv = createCanvas(width, height)
   cnv.mouseClicked(getWaypoint);
 
-  // angleMode(RADIANS);
-  // spawnSoldier(width / 2, height / 2, "Tommy");
-  // spawnSoldier(width/2-100,height/2-100,"Billy")
-  //console.log(gameObject)
-  spawnSquad(squadSize, width / 2, height-250, 'friendly');
+  spawnSquad(squadSize, width / 2, height - 250, 'friendly');
+
   spawnSquad(squadSize, width / 2, 0, 'enemy');
-  console.log(gameObject)
+  //console.log(gameObject)
 
 };
 
@@ -343,18 +376,14 @@ function draw() {
 
 
 
- // console.log(gameObject)
+  // console.log(gameObject)
   //let squadMates = gameObject.squadMates;
 
   // //decide here which game objects you want to render
 
   for (let i = 0; i < gameObject.world.length; i++) {
-    // let faction = gameObject.factions[i];
-   // console.log(gameObject[faction])
- 
-      gameObject.world[i].behaviour();
-      gameObject.world[i].draw();
-
+    gameObject.world[i].behaviour();
+    gameObject.world[i].draw();
   }
   waypoint.draw();
 
@@ -370,9 +399,6 @@ function getWaypoint() {
   gameObject.waypointX = mouseX;
   gameObject.waypointY = mouseY;
   waypointTimestamp = new Date().getTime()
-
-  // if the waypoint is on top of you, you have been selected
-
 }
 
 
@@ -384,6 +410,5 @@ function pickSmallestByMagnitude(a, b, c) {
   if (smallest === magA) { return a; };
   if (smallest === magB) { return b; };
   if (smallest === magC) { return c; };
-  //console.log("check pickSmallestByMagnitude() function")
   return;
 }
